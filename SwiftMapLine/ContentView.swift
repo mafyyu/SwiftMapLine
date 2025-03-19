fca2fa43c61d75dcbf74570209ae07e98a879e01
mafyyu <morita0126m@gmail.com>
2025-03-19 21:53:18 +0900

    Initial Commit

---
+++ SwiftMapLine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftMapLine
+//
+//  Created by 森田将嵩 on 2025/03/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftMapLine/SwiftMapLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMapLineApp.swift
+//  SwiftMapLine
+//
+//  Created by 森田将嵩 on 2025/03/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMapLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
